63a74318d693555862270b3b2b0f4af739da6c6e
Алекс <@DESKTOP-MVPJD5D>
2019-12-12 23:08:01 +0300

    Began implementing simple output

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,5 +1,13 @@
+mov ah, 0x0e
+
+mov al, [msg]
+int 0x10
+
 loop:
     jmp loop
+
+msg: db "This is quite simple OS", 0
+smb: db "X"
 
 times 510-($-$$) db 0
 dw 0xaa55
---
+++ qemu-x86-64_boot.bat
@@ -0,0 +1,1 @@
+qemu-system-x86_64.exe boot_sect.bin
