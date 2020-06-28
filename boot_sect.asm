54169d1a6de38fdc49f4955cf59b75b18f94fbde
Khrenov <khrenov.a@office.lan>
2020-06-28 17:07:44 +0300

    Added print functions to boot sector

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,13 +1,45 @@
-mov ah, 0x0e
+[org 0x7c00]
 
-mov al, [msg]
-int 0x10
+mov bx, msg
 
-loop:
-    jmp loop
+call print_string_function
 
-msg: db "This is quite simple OS", 0
-smb: db "X"
+jmp $
+
+; data
+msg: db 'Simple operation system', 0
+chart: db 't'
+
+; functions
+print_char_function:
+    pusha
+
+    mov ah, 0x0e
+    mov al, [bx]
+    int 0x10
+
+    popa
+    ret
+
+print_string_function:
+    pusha
+    
+    start:
+        mov ah, 0x0e
+        mov al, [bx]
+
+        cmp ax, 0x0e00
+        je end
+
+        int 0x10
+        add bl, 0x01
+
+        jmp start
+
+    end:
+        popa
+        ret
 
 times 510-($-$$) db 0
 dw 0xaa55
+
---
+++ run.sh
@@ -0,0 +1,5 @@
+rm boot_sect.bin
+
+nasm boot_sect.asm -f bin -o boot_sect.bin
+
+qemu-system-i386 boot_sect.bin
