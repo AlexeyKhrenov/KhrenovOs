609fc8d29ae8872585091977c909aa0c0cab2318
khrenov.a <khrenov.a@office.lan>
2020-07-07 22:10:52 +0300

    Added Kernel entry routine

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 rm kernel.o
 rm kernel.bin
 
+nasm kernel_entry.asm -f elf -o kernel_entry.o
 gcc -ffreestanding -fno-asynchronous-unwind-tables -fno-pie -m32 -c kernel.c -o kernel.o
-ld -o kernel.bin -m elf_i386 --oformat binary kernel.o
+ld -o kernel.bin -m elf_i386 --oformat binary kernel_entry.o kernel.o
 ndisasm -b 32 kernel.bin > kernel.dis
--- kernel.c
+++ kernel.c
@@ -1,3 +1,7 @@
+void some_function(){
+    
+}
+
 void main(){
     char* video_memory_ptr = (char*) 0xb8000;
     *video_memory_ptr = 'X';
